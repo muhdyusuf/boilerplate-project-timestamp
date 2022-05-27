fc84a821dc4d2874a4853e55dbfa308ed82d1573
muhdyusuf <92169759+muhdyusuf@users.noreply.github.com>
2022-05-28 07:09:15 +0800

    free code camp backend project 1

--- index.js
+++ index.js
@@ -14,19 +14,78 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res)=> {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res)=> {
   res.json({greeting: 'hello API'});
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
+
+app.get("/api/:date?",(req,res)=>{
+ 
+  let date=req.params.date
+  
+
+  if(!/[\s-]/g.test(date) && date!==undefined){
+    date=parseInt(date)
+  }
+  
+
+  const days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+  let months= ["Jan","Feb","Mar","April","May","June","July","August","September","October","November","December"];
+
+  months=months.map(item=>item.slice(0,3))
+
+   const _date=date===undefined? new Date :new Date(date)
+   
+
+   if(_date=="Invalid Date"){
+     res.send({error:"Invalid Date"})
+     return
+   }
+   
+  
+ 
+
+  // const day=days[_date.getDay()]
+  // const month=months[_date.getMonth()]
+  // const year=_date.getFullYear()
+
+  // function timeFormatter(val){
+  //   if(val>=10)return val
+  //   return `0${val}`
+  // }
+  
+
+
+
+
+ 
+  console.log(_date.getHours())
+  const output={
+    unix:_date.getTime(),
+    // utc:`${day}, ${_date.getDay()} ${month} ${year} ${timeFormatter(_date.getHours())}:${timeFormatter(_date.getMinutes())}:${timeFormatter(_date.getSeconds())} GMT`
+    utc:_date.toUTCString()
+
+  }
+  console.log(_date)
+
+  res.send(output)
+
+
+  
+  
+  
+  
+
+})
